950fc5cfcc396418a2207522bd3e622d8f391b5c
Atul Saini <atulsaini9886@gmail.com>
2021-06-13 10:24:20 +0530

    Added Store and dispatch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
-export default function reducer(action, state=[]) {
+export default function movies (state=[] , action) {
 
     if(action.type === 'ADD_MOVIES'){
         return action.movies;
     }
-    return State ;
-;}
+    return state ;
+}
