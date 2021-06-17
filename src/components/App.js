b114c144963bde2b69463ee8b7b5549c27cbedfa
Atul Saini <atulsaini9886@gmail.com>
2021-06-17 10:34:46 +0530

    Added AppWrappers and connect property of react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import {data} from '../data';
 import NavBar from './NavBar';
 import MovieCard from './MovieCard';
 import { addMovies, displyFav } from "../actions";
+import {StoreContext} from '../index';
 
 class App extends React.Component {
   
@@ -38,29 +39,43 @@ class App extends React.Component {
    
    console.log(this.props.store.getState());
    const displayList = showFav ? favourites : list;
+   
    return (
-      <div className="App">
-        <NavBar 
-          dispatch = {this.props.store.dispatch}
-          search = {search}
-        />
-        <div className="main">
-          <div className="tabs">
-            <div className={`tab ${showFav ? '' : 'active-tabs'}`} onClick={()=>this.clickedFav(false)}>Movies</div>
-            <div className={`tab ${showFav ? 'active-tabs': ''}`}  onClick={()=>this.clickedFav(true)}>Favourites</div>
-          </div>
+    <div className="App">
+      <NavBar 
+        dispatch = {this.props.store.dispatch}
+        search = {search}
+      />
+      <div className="main">
+        <div className="tabs">
+          <div className={`tab ${showFav ? '' : 'active-tabs'}`} onClick={()=>this.clickedFav(false)}>Movies</div>
+          <div className={`tab ${showFav ? 'active-tabs': ''}`}  onClick={()=>this.clickedFav(true)}>Favourites</div>
+        </div>
 
-          <div className="list">
-            {displayList.map((movie,index)=>{
-              return <MovieCard movie={movie}
-              key={`movies-${index}`}
-              dispatch={this.props.store.dispatch}
-              isFav = {this.isMovieFav(movie)}
-            />
-            })}
-          </div>
+        <div className="list">
+          {displayList.map((movie,index)=>{
+            return <MovieCard movie={movie}
+            key={`movies-${index}`}
+            dispatch={this.props.store.dispatch}
+            isFav = {this.isMovieFav(movie)}
+          />
+          })}
         </div>
       </div>
+    </div>
+   );
+  } 
+}
+
+class AppWrapper extends React.Component{
+  render(){
+    return (
+      <StoreContext.Consumer>
+        {(store)=>{
+          <App store={store}/>
+         }
+        }
+      </StoreContext.Consumer>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 
@@ -21,15 +21,29 @@ const thunk = ({dispatch, getState}) => (next) => (action) => {
 
 
 const store = createStore(rootReducer,applyMiddleware(logger, thunk));
-
+export const StoreContext = createContext(); 
 // store.dispatch({
 //   type: 'ADD_MOVIES',
 //   movies: [{name: 'Superman'}]
 // })
 
+
+class Provider extends React.Component{
+  render(){
+    const {store} = this.props;
+    return(
+    <StoreContext.Provider value={store}>
+      {this.props.children}
+    </StoreContext.Provider>  
+    );
+  }  
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App store={store}/>
+    <Provider store={store}>
+      <App store={store}/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
